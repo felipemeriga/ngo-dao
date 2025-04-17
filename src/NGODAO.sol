7461a048b21c1529ae564c7544fac66a216bf94b
Felipe Ramos da Silva <felipe.silva@fox.com>
2025-04-17 16:53:53 +0900

    feat: adding new function clearProposals

--- src/NGODAO.sol
+++ src/NGODAO.sol
@@ -7,17 +7,17 @@ import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 
 // Define the structure for a proposal.
-struct Proposal {
-    string title;
-    string description; // Description of the donation proposal.
-    address target; // Target address to call.
-    uint256 value; // Amount of ETH (in wei) to send.
-    bytes data; // Call data (could be empty for a plain ETH transfer).
-    uint256 deadline; // Voting deadline timestamp.
-    uint256 yesVotes; // Count of yes votes.
-    uint256 noVotes; // Count of no votes.
-    bool executed; // Whether the proposal has been executed.
-}
+    struct Proposal {
+        string title;
+        string description; // Description of the donation proposal.
+        address target; // Target address to call.
+        uint256 value; // Amount of ETH (in wei) to send.
+        bytes data; // Call data (could be empty for a plain ETH transfer).
+        uint256 deadline; // Voting deadline timestamp.
+        uint256 yesVotes; // Count of yes votes.
+        uint256 noVotes; // Count of no votes.
+        bool executed; // Whether the proposal has been executed.
+    }
 
 contract NGODAO is Initializable, OwnableUpgradeable, UUPSUpgradeable {
     // Array to store proposals.
@@ -54,6 +54,13 @@ contract NGODAO is Initializable, OwnableUpgradeable, UUPSUpgradeable {
         __Ownable_init(msg.sender);
         __UUPSUpgradeable_init();
         votingPeriod = _votingPeriod;
+    }
+
+    /**
+     * @notice Clear all existing proposals
+     */
+    function clearProposals() external onlyOwner {
+        delete proposals;
     }
 
     /**
@@ -97,8 +104,8 @@ contract NGODAO is Initializable, OwnableUpgradeable, UUPSUpgradeable {
      * @return proposalId The ID of the newly created proposal.
      */
     function createProposal(string memory _title, string memory _description, address _target, uint256 _value, bytes memory _data)
-        public
-        returns (uint256 proposalId)
+    public
+    returns (uint256 proposalId)
     {
         require(address(this).balance >= _value, "DAO treasury doesn't have enough funds");
         uint256 deadline = block.timestamp + votingPeriod;
